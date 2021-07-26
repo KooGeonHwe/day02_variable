e5bf3e9bb95ac349d15050c6aca2b805bd9bd4e8
Koo Geonhwe <mashrjs@naver.com>
2021-07-26 12:43:21 +0900

    기본 별수의 이해

---
+++ src/day02_variable/Ex02.java
@@ -0,0 +1,13 @@
+package day02_variable;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		int age = 25;
+		double weight = 50.0;
+		double height = 163.5;
+		System.out.println("나의 나이는" +age+"세 입니다");
+		System.out.println("나의 나이는" +weight+"세 입니다");
+		System.out.println("나의 나이는" +height+"세 입니다");
+	}
+
+}
